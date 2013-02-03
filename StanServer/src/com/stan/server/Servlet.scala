fe8d4ebec8d25c6e03a96a2aff80ad9db4fec135
Grant Heywood <grantheywood@gmail.com>
2013-02-03 18:03:14 +0000

    sdgds

--- StanServer/src/com/stan/server/Servlet.scala
+++ StanServer/src/com/stan/server/Servlet.scala
@@ -3,6 +3,7 @@ package com.stan.server
 import com.codahale.jerkson._
 import org.scalatra._
 import scalate.ScalateSupport
+import java.util.Date
 
 
 /**
@@ -12,28 +13,49 @@ import scalate.ScalateSupport
 
 class Servlet extends ScalatraServlet with ScalateSupport
 {
+  var newPlan = new Plan("A New Plan", "This is a new Plan")
+  var newTask = new Task("NewTask", "This is a new Task", "Everywhere", 1, 3, "Something", new Date(), new Date())
+
+  newPlan.addTask(newTask)
 
   get("/")
   {
 
   }
 
+  // Returns all plans
+  get("/plan")
+  {
+    // Send back all the plans in JSON format
+
+  }
+
+  // Returns the identified Plan
   get("/plan/:id")
   {
 
   }
 
+  // Returns all the Tasks
+  get("/task/")
+  {
+
+  }
+
+  // Returns the identified Task
   get("/task/:id")
   {
 
   }
 
+  // Adds a new Plan
   post("/addplan/:new")
   {
 
 
   }
 
+  // Adds a new Task
   post("/addtask/:new")
   {
 
