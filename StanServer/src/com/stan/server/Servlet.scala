91146205a13c44ccf0d987b71f3938bc39a6720a
Grant Heywood <grantheywood@gmail.com>
2013-02-03 15:43:34 +0000

    Servlet Playing

--- StanServer/src/com/stan/server/Servlet.scala
+++ StanServer/src/com/stan/server/Servlet.scala
@@ -1,6 +1,14 @@
 package com.stan.server
 
+import com.codahale.jerkson._
 
-class Servlet {
+/**
+ * The Servlet class, handles HTTP requests and decides what to do with them!
+ *
+ */
+
+class Servlet
+{
+
 
 }
---
+++ StanServer/src/com/stan/server/Task.scala
@@ -0,0 +1,13 @@
+package com.stan.server
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: grantheywood
+ * Date: 03/02/2013
+ * Time: 15:33
+ * Holds the tasks...
+ */
+class Task
+{
+
+}
